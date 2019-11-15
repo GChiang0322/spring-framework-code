2387e52f0d7620be15d76c28d774d2d12bb36470
LCY <646787366@qq.com>
2019-11-15 14:37:40 +0800

    spring源码编译成功第一次提交 2019年11月15日14:27:43

---
+++ spring-yang/src/test/java/com/yang/test/Test.java
@@ -0,0 +1,16 @@
+package com.yang.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @Auther: LCY
+ * @Date: 2019/11/15 14:10
+ * @Description:
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext annotationConfigApplicationContext=new AnnotationConfigApplicationContext();
+		System.out.println("==========");
+	}
+}
